2aacae31dbc9dd76b0ddf167690384e44b4e00ac
Joniator <jonathanboeckel1996@gmail.com>
2016-08-31 22:40:08 +0200

    Added missing coma

--- spotify_ripper/main.py
+++ spotify_ripper/main.py
@@ -289,7 +289,7 @@ def main(prog_args=sys.argv[1:]):
     parser.add_argument(
         '--playlist-m3u', action='store_true',
         help='create a m3u file when ripping a playlist')
-    parser.add_argument('--kodi-m3u', action='store_true'
+    parser.add_argument('--kodi-m3u', action='store_true',
         help='creates the m3u file in OSMC/kodis default playlist dir (~/.kodi/userdata/playlists/music)')
     parser.add_argument(
         '--absolute-m3u', action='store_true',
